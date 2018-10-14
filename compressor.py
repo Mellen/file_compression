6b7759e7bc4e4dfa278e09f3ce66fa4d6b1541b5
Matt Ellen <matt@matthewellen.co.uk>
2018-10-14 22:27:40 +0100

    another failure

--- compressor.py
+++ compressor.py
@@ -10,40 +10,34 @@ class Compressor():
     def compress(self):
         with open(self.filename, 'r') as textfile:
             text = textfile.read()
-        
-        uniques = {t:i for (i, t) in enumerate(set(text))}
-        data = self.text_to_data(text, uniques)
+
+        frequencies = {}
+        for c in text:
+            if c in frequencies:
+                frequencies[c] += 1
+            else:
+                frequencies[c] = 1
+
+        freq_list = sorted([(k,frequencies[k]) for k in frequencies], key=lambda f: f[1])
+
+        frequencies = {f[0]:'0'*i for i, f in enumerate(freq_list)}
+
+        data = self.text_to_data(text, frequencies)
+        data_byte_count = (data.bit_length() // 8) + 1;
         
         with open(self.outputname, 'wb') as compressed_file:
             filename_bytes = os.path.basename(self.filename).encode('utf-8')
             compressed_file.write((len(filename_bytes)).to_bytes(4, sys.byteorder))
             compressed_file.write(filename_bytes)
-            unique_pickle = pickle.dumps(uniques)
-            compressed_file.write((len(unique_pickle)).to_bytes(4, sys.byteorder))
-            compressed_file.write(unique_pickle)
-            bit_length = len(uniques).bit_length()
-            compressed_file.write(bit_length.to_bytes(4, sys.byteorder))
-            compressed_file.write((len(data)).to_bytes(4, sys.byteorder))
-            for datum in data:
-                compressed_file.write(datum.to_bytes(8, sys.byteorder))
+            freq_pickle = pickle.dumps(frequencies)
+            compressed_file.write((len(freq_pickle)).to_bytes(4, sys.byteorder))
+            compressed_file.write(freq_pickle)
+            compressed_file.write(data_byte_count.to_bytes(16, sys.byteorder))
+            compressed_file.write(data.to_bytes(data_byte_count, sys.byteorder))
 
-    def text_to_data(self, text, uniques):
-        bl = len(uniques).bit_length()
-        max_inserts = 64//bl;
-        current_insert = 0
-        value = ''
-        values = []
+    def text_to_data(self, text, frequencies):
+        value = '1'
         for c in text:
-            n = uniques[c]
-            b = (bin(n)[2:]).zfill(bl)
-            value += b
-            current_insert += 1
-            if current_insert == max_inserts:
-                values.append(int(value, 2))
-                current_insert = 0
-                value = ''
-
-        if current_insert < max_inserts and current_insert > 0:
-            values.append(int(value, 2))
-
-        return values
+            value += frequencies[c]
+            value += '1'
+        return int(value, 2)
--- decompressor.py
+++ decompressor.py
@@ -9,30 +9,17 @@ class Decompressor():
         with open(self.filename, 'rb') as compressed_file:
             filename_length = int.from_bytes(compressed_file.read(4), sys.byteorder)
             filename = compressed_file.read(filename_length).decode('utf-8')
-            ul = int.from_bytes(compressed_file.read(4), sys.byteorder)
-            print('ul', ul)
-            uniques = pickle.loads(compressed_file.read(ul))
-            bit_length = int.from_bytes(compressed_file.read(4), sys.byteorder)
-            dl = int.from_bytes(compressed_file.read(4), sys.byteorder)
-            data = []
-            current_bytes = compressed_file.read(8)
-            while current_bytes:
-                datum = int.from_bytes(current_bytes, sys.byteorder)
-                data.append(datum)
-                current_bytes = compressed_file.read(8)
+            fl = int.from_bytes(compressed_file.read(4), sys.byteorder)
+            frequencies = pickle.loads(compressed_file.read(fl))
+            print(frequencies)
+            dl = int.from_bytes(compressed_file.read(16), sys.byteorder)
+            data = int.from_bytes(compressed_file.read(dl), sys.byteorder)
 
         with open(filename, 'w') as textfile:
             output = ''
-            reverse_lookup = {uniques[key]:key for key in uniques}
-            expected_length = (64 // bit_length)*bit_length
-            for value in data:
-                b = bin(value)[2:]
-                b = b.zfill(expected_length)
-                while len(b) >= bit_length:
-                    chunk = b[:bit_length]
-                    b = b[bit_length:]
-                    key = int(chunk, 2)
-                    output += reverse_lookup[key]
-
+            reverse_lookup = {frequencies[key]:key for key in frequencies}
+            full_binary = bin(data)[3:].split('1')
+            for n in full_binary:
+                output += reverse_lookup[n]
             textfile.write(output)
                 
