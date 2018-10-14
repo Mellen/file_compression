39eb07c379164af46e25286289d0c39120ce38e3
Matt Ellen <matt@matthewellen.co.uk>
2018-10-14 19:00:32 +0100

    compressed into numbers. still not compressed

--- compressor.py
+++ compressor.py
@@ -8,16 +8,39 @@ class Compressor():
     def compress(self):
         with open(self.filename, 'r') as textfile:
             text = textfile.read()
-        split_text = text.split(' ')
-        uniques = {t:chr(i) for (i, t) in enumerate(set(split_text))}
-        data = ''.join([uniques[t] for t in split_text])
-        compressed = CompressedStructure(self.filename, uniques, data)
+        
+        uniques = {t:i for (i, t) in enumerate(set(text))}
+        data = self.text_to_data(text, uniques)
+        compressed = CompressedStructure(self.filename, uniques, len(uniques).bit_length(), data)
+        
         with open(self.outputname, 'wb') as compressed_file:
             pickle.dump(compressed, compressed_file)
 
+    def text_to_data(self, text, uniques):
+        bl = len(uniques).bit_length()
+        max_inserts = 64//bl;
+        current_insert = 0
+        value = ''
+        values = []
+        for c in text:
+            n = uniques[c]
+            b = (bin(n)[2:]).zfill(bl)
+            print(n, b)
+            value += b
+            current_insert += 1
+            if current_insert == max_inserts:
+                values.append(int(value, 2))
+                current_insert = 0
+                value = ''
+
+        if current_insert < max_inserts:
+            values.append(int(value, 2))
+
+        return values
 
 class CompressedStructure():
-    def __init__(self, filename, uniques, data):
+    def __init__(self, filename, uniques, bit_length, data):
         self.filename = filename
         self.uniques = uniques
+        self.bit_length = bit_length
         self.data = data
--- decompressor.py
+++ decompressor.py
@@ -9,12 +9,18 @@ class Decompressor():
             compressed = pickle.load(compressed_file)
 
         with open(compressed.filename, 'w') as textfile:
-            output = []
+            output = ''
             reverse_lookup = {compressed.uniques[key]:key for key in compressed.uniques}
-            for c in compressed.data:
-                output.append(reverse_lookup[c])
+            expected_length = (64 // compressed.bit_length)*compressed.bit_length
+            for value in compressed.data:
+                b = bin(value)[2:]
+                b = b.zfill(expected_length)
+                while len(b) >= compressed.bit_length:
+                    chunk = b[:compressed.bit_length]
+                    print(chunk, int(chunk,2))
+                    b = b[compressed.bit_length:]
+                    key = int(chunk, 2)
+                    output += reverse_lookup[key]
 
-            text = ' '.join(output)
-
-            textfile.write(text)
+            textfile.write(output)
                 
